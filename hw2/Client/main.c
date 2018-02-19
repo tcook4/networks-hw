037ca7e04cecef91ea17ea16ee3414388fbd8931
Tyler Cook <tyler.b.cook4@gmail.com>
2018-02-19 13:40:41 -0600

    imported hw1 code for edits

--- hw2/Client/main.c
+++ hw2/Client/main.c
@@ -1,7 +1,130 @@
-#include <stdio.h>
+/* Written by Tyler Cook
+ * UNT CSCE 3530
+ * Homework 2
+ * February 28th, 2018
+ * Description:
+ */
 
-int main(int argc, char *argv[])
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char **argv)
 {
+    int sockfd;                     // Socket file descriptor
+    int n;                          // Error checking for read() and write() calls
+    struct sockaddr_in servaddr;    // Server address structure
+    char buffer[1024];              // Sent and received message buffer
+    char input[1024];               // fgets() input buffer
+    int dataLength;                 // Length of the data to be sent
+    int bufferLength;               // Length of the buffer to be read to
+    int portNumber;                 // Port number
+    int quit;                       // Quitting detection
+
+    // Initialization
+    quit = 0;
+
+    // Verify we have correct number of arguments
+    if (argc != 2)
+    {
+        printf("Error: Program usage: %s port_number\n", argv[0]);
+        exit(1);
+    }
+    else
+    {
+        portNumber = atoi(argv[1]);
+    }
+
+    // AF_INET - IPv4 IP , Type of socket, protocol
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) // check for errors
+    {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;
+    servaddr.sin_port=htons(portNumber); // Server port number
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    inet_pton(AF_INET,"129.120.151.94",&(servaddr.sin_addr));
+
+    // Connect to the server
+    if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
+    {
+        perror("Error connecting\n");
+        exit(1);
+    }
+
+    printf("Successfully connected to server!\n");
+    printf("Please enter a string to parse, or \"quit\" to exit\n");
+
+    // Main body loop
+    // Ask user for input then display server response
+    while (1)
+    {
+        // Get input from user
+        printf("Input: ");
+        bzero(input, 1024);
+        fgets(input, 1024, stdin);
+
+        // Check if we're quitting
+        if (strcmp(input, "quit\n") == 0)
+        {
+            quit = 1;
+        }
+
+        // Send data to server
+        // Prepare our buffer
+        bzero(buffer, 1024);
+        strcpy(buffer, input);
+
+        // Find and send the size of the message after converting to network order
+        bufferLength = htonl(strlen(buffer));
+        n = write(sockfd, (char*)&bufferLength, sizeof(bufferLength));
+        if (n < 0)
+        {
+            perror("Error sending message size\n");
+        }
+
+        // Send the actual message body
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            perror("Error sending message\n");
+        }
+
+        // We can quit now
+        if (quit == 1)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+
+        // Listen for server response
+        // Receive response size and convert from network order
+        n = read(sockfd, (char*)&bufferLength, sizeof(bufferLength));
+        if (n < 0)
+        {
+            perror("Error receiving message size\n");
+        }
+        dataLength = ntohl(bufferLength);
+
+        // Zero our buffer and read the message body
+        bzero(buffer, 1024);
+        n = read(sockfd, buffer, dataLength);
+        if (n < 0)
+        {
+            perror("Error receiving message\n");
+        }
+
+        // Print server response to the user
+        printf("Server response: \n");
+        printf("%s\n", buffer);
+    }
 
     return 0;
 }
--- hw2/Server/main.c
+++ hw2/Server/main.c
@@ -1,7 +1,179 @@
-#include <stdio.h>
+/* Written by Tyler Cook
+ * UNT CSCE 3530
+ * Homework 1
+ * January 29th, 2018
+ * Description: This program is the server side to a client. The client sends messages which the server accepts,
+ * then returns the word count and converts to message to lower case.
+ */
 
-int main(int argc, char *argv[])
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
 {
-    printf("Hello World!\n");
+    char buffer[1024];                  // Communication buffer between client and server
+    int listen_fd, conn_fd, n;          // File descriptors and error checking
+    struct sockaddr_in servaddr;        // Server address structure
+    char output[1024];                  // Output message storage
+    int words, characters, i;           // Data storage for response message and iterator
+    int bufferLength;                   // Length of message to be sent
+    int dataLength;                     // Length of message to be received
+    int portNumber;                     // Port number to use if supplied
+    char numBuff[12];                   // Number storage for casting int to string
+    int foundLetter;                    // Multiple space detection
+
+    // Initialization
+    words = 0;
+    characters = 0;
+    foundLetter = 0;
+
+    // Verify we have correct number of arguments
+    if (argc != 2)
+    {
+        printf("Error: Program usage: %s port_number\n", argv[0]);
+        exit(1);
+    }
+    else
+    {
+        portNumber = atoi(argv[1]);
+    }
+
+    // AF_INET - IPv4 IP , Type of socket, protocol
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+    servaddr.sin_port = htons(portNumber);
+
+    // Binds the above details to the socket
+    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    // Start listening to incoming connections
+    listen(listen_fd, 10);
+    printf("Listening for connection...\n");
+
+    // Accept an incoming connection
+    conn_fd = accept(listen_fd, (struct sockaddr*)NULL, NULL);
+    if (conn_fd < 0) // verify we accepted correctly
+    {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+    printf("Successfully connected to client!\n");
+
+    // Main body loop
+    // Receive message from user, strip capitalization and return new string along with
+    // word and character count
+    while(1)
+    {
+        // Get input from client
+        // Get message size and convert from network order
+        n = read(conn_fd, (char*)&bufferLength, sizeof(bufferLength));
+        if (n < 0)
+        {
+            perror("Error getting data size\n");
+            exit(1);
+        }
+        dataLength = ntohl(bufferLength);
+
+        // Read the actual message
+        bzero(buffer, 1024);
+        n = read(conn_fd, buffer, dataLength);
+        if (n < 0)
+        {
+            perror("Error reading message from client\n");
+            exit(1);
+        }
+
+        // Check if we're quitting
+        if (strcmp(buffer, "quit\n") == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+
+        // Parse our string for capitals, words and characters, and append it to our response
+        // Subtract one from strlen(buffer) to remove newline
+        bzero(output, 1024);
+        for (i = 0; i < strlen(buffer)-1; i++)
+        {
+            // Add spaces and prepare to add next found word
+            if (buffer[i] == ' ')
+            {
+                foundLetter = 0;
+                characters++;
+                output[i] = ' ';
+            }
+
+            // Remove any capitalization
+            else if (buffer[i] >= 'A' && buffer[i] <= 'Z')
+            {
+                if (foundLetter == 0) // Increment words if we're coming from a space
+                {
+                    words++;
+                    foundLetter = 1;
+                }
+                output[i] = buffer[i] + 32;
+                characters++;
+            }
+
+            // Add any lowercase as-is
+            else if (buffer[i] >= 'a' && buffer[i] <= 'z')
+            {
+                if (foundLetter == 0)
+                {
+                    words++;
+                    foundLetter = 1;
+                }
+                output[i] = buffer[i];
+                characters++;
+            }
+            else
+            {
+                characters++;
+                output[i] = buffer[i];
+            }
+        }
+
+        // Generate the rest of our response information
+        strcat(output, "\nTotal Characters: ");
+        sprintf(numBuff, "%d", characters);
+        strcat(output, numBuff);
+        strcat(output, "\nTotal Words: ");
+        sprintf(numBuff, "%d", words);
+        strcat(output, numBuff);
+        strcat(output, "\n");
+        strcpy(buffer, output);
+
+        // Send client the message size
+        bufferLength = htonl(strlen(buffer));
+        n = write(conn_fd, (char*)&bufferLength, sizeof(bufferLength));
+        if (n < 0)
+        {
+            perror("Error sending client data size\n");
+            exit(1);
+        }
+
+        // Write actual message to the client
+        n = write(conn_fd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            perror("Error sending client message\n");
+            exit(1);
+        }
+
+        // Inform user of successful send and reset counters
+        printf("Successfully sent message\n");
+        characters = 0;
+        words = 0;
+        foundLetter = 0;
+    }
+
+    close (conn_fd); // Close the connection
     return 0;
 }
